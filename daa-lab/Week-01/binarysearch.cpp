93918478d73a4106242078ee35ea3054dc86dcfd
Gautambisht86525 <158544119+Gautambisht86525@users.noreply.github.com>
2026-02-13 18:12:03 +0530

    Create binarysearch.cpp

---
+++ daa-lab/Week-01/binarysearch.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        int target;
+        cin >> target;
+
+        int ans = binarySearch(arr, n, target);
+
+        if (ans != -1)
+            cout << ans << endl;
+        else
+            cout << -1 << endl;
+    }
+
+    return 0;
+}
