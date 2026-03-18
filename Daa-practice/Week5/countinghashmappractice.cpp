21f18828754b6c6e236c351cf143d0832c07a35f
Gautambisht86525 <158544119+Gautambisht86525@users.noreply.github.com>
2026-03-18 23:55:36 +0530

    Create countinghashmappractice.cpp

---
+++ Daa-practice/Week5/countinghashmappractice.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    unordered_map<int,int> freq;
+
+    for(int i=0;i<n;i++){
+        freq[arr[i]]++;
+    }
+
+    for(auto it:freq){
+        cout<<it.first<<"-> "<<it.second<<endl;
+    }
+
+
+}
